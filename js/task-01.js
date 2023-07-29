ee52bd197eb58cd980d88d30fa524ff3653c6eca
evgen122 <evg.khristenko@gmail.com>
2023-07-29 08:48:26 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -5,8 +5,15 @@ const listWithId = document.querySelector("#categories");
 const listWithClass = listWithId.querySelectorAll(".item");
 console.log(`Number of categories: ${listWithClass.length}`);
 
+// listWithClass.forEach(function (item) {
+//   let nameCategory = item.querySelector("h2").textContent;
+//   console.log(`Category: ${nameCategory}`);
+//   console.log(`Elements: ${item.querySelectorAll("li").length}`);
+// });
+
 listWithClass.forEach(function (item) {
-  let nameCategory = item.querySelector("h2").textContent;
-  console.log(`Category: ${nameCategory}`);
+  let nameFirstChild = item.firstElementChild.textContent;
+
+  console.log(`Category: ${nameFirstChild}`);
   console.log(`Elements: ${item.querySelectorAll("li").length}`);
 });
