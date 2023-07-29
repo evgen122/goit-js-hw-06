22d666282eb5a24b38e785599a8bf9e431d00548
evgen122 <evg.khristenko@gmail.com>
2023-07-29 09:41:14 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,25 @@
+/** @format */
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+const el = [];
+
+ingredients.map((item) => {
+  const nameIngredient = document.createElement("li");
+  nameIngredient.textContent = item;
+  nameIngredient.classList.add("item");
+  el.push(nameIngredient);
+  // console.log(nameIngredient);
+});
+// console.log(el);
+
+list.prepend(el[0], el[1], el[2], el[3], el[4], el[5]);
+console.log(list);
