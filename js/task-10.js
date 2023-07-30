263c2362cb9123d3abf5a382f6bcb4813dabf61a
evgen122 <evg.khristenko@gmail.com>
2023-07-30 19:52:45 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,15 +1,25 @@
 /** @format */
 
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
 const elements = {
-  buttonCreate: document.querySelector('button[data-create="Create"]'),
-  buttonDestroy: document.querySelector('button[data-destroy="Destroy"]'),
-  // button: document.querySelector("button"),
+  buttonCreate: document.querySelector("button[data-create]"),
+  buttonDestroy: document.querySelector("button[data-destroy]"),
+  inputNumber: document.querySelector("input"),
   // span: document.querySelector(".color"),
 };
 
-// createBoxes(amount);
+// console.dir(elements.buttonCreate);
+// console.log(elements.buttonDestroy);
+// console.log(elements.inputNumber);
+
+elements.buttonCreate.addEventListener("click", createBoxes);
+
+function createBoxes(amount) {
+  amount = elements.inputNumber.value;
+  // console.log(amount);
+}
