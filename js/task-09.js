bb7f0d9f9c44e699e625fd8135c3547591c84381
evgen122 <evg.khristenko@gmail.com>
2023-07-30 18:14:06 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,19 @@
+/** @format */
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+const elements = {
+  body: document.querySelector("body"),
+  button: document.querySelector("button"),
+  span: document.querySelector(".color"),
+};
+
+elements.button.addEventListener("click", handlerClickChangeColor);
+
+function handlerClickChangeColor() {
+  elements.body.style.backgroundColor = getRandomHexColor();
+  elements.span.textContent = elements.body.style.backgroundColor;
 }
