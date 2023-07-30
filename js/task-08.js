0b20815f0f99ac73dfd461e07a4a77ae4ac35046
evgen122 <evg.khristenko@gmail.com>
2023-07-30 10:32:14 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+/** @format */
 
+const elements = {
+  form: document.querySelector("form"),
+};
+
+elements.form.addEventListener("submit", handlerSubmit);
+
+function handlerSubmit(evt) {
+  evt.preventDefault();
+
+  console.dir(evt.currentTarget);
+
+  const {email, password} = evt.currentTarget.elements;
+
+  console.log(email);
+  console.log(email.v);
+  console.log(password);
+}
